b3b4c98e412cd1e6094253e1e6ac5a18c684183e
Aakash Thanigaivel <aakash.thanigaivel@photon.com>
2026-06-27 00:17:30 +0530

    slideshowmusiq

--- src/pages/SlideshowPage.jsx
+++ src/pages/SlideshowPage.jsx
@@ -1,8 +1,10 @@
+import BackgroundMusic from '../components/BackgroundMusic'
 import Slideshow from '../components/slideshow'
 
 export default function SlideshowPage() {
   return (
     <div className="relative h-[100dvh] overflow-hidden text-[#FDF6EC]">
+      <BackgroundMusic />
       <Slideshow />
     </div>
   )
